fb5404bc7ade927a74eb08352da0cf4e6d18a370
abbottcd <chrisabbott1@gmail.com>
2024-11-30 15:56:21 -0500

    Update main.cs
    added basic dictionary of words and an initial game loop

--- main.cs
+++ main.cs
@@ -1,3 +1,5 @@
+// C# Guess a letter
+
 // pseudo code: 
 // setup:
 // dictionary of words
@@ -16,3 +18,68 @@
 // pick again
 //
 // end game routine 
+
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Game Intro and setup -- enter name a generate a random number 
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName);
+            Console.WriteLine("Welcome to Hangman!  I'll pick a random word from the dictionary and you'll have 10 chances to guess my word.  Ready begin!");
+
+            // adding a for loop
+            for (int i = 0; i < 10; i++)
+            {
+            
+            
+            // adding a random number and assigning an array
+            Random rnd = new Random();
+            int rand1 = rnd.Next(8);
+
+
+            string[] letters = {"mushroom", "guacamole", "computer", "linguine", "television", "calendar", "exaggerate", "football"};
+            Console.WriteLine(letters[rand1]);
+
+            
+            string answer = (letters[rand1]);
+            string userGuess = "";
+            int guesses = 0;
+
+            // prompt the user for an initial guess 
+            Console.WriteLine(userName + ", please type letter " + answer + ":");
+           
+            // begin the game loop
+            while (userGuess != answer)
+            {
+                userGuess = Console.ReadLine();
+                guesses++;
+                
+                if (userGuess != answer)
+                {
+                    Console.WriteLine("Please try again");
+                }
+                
+            }
+            
+            // game ends
+           Console.WriteLine("Congrats! You guessed it right.");
+           Console.WriteLine("It took you " + guesses + " guesses to get it correct!");
+           Console.WriteLine("=========================");
+           
+            }
+        
+        Console.WriteLine("Well Done! Game Over.");    
+        
+        }
+    
+    }
+            
+}
