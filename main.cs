9c427bd5ab8d392aacfe224f9945375f0a7083c2
abbottcd <chrisabbott1@gmail.com>
2024-11-30 15:42:36 -0500

    Create main.cs
    file creation with psudo-code

---
+++ main.cs
@@ -0,0 +1,18 @@
+// pseudo code: 
+// setup:
+// dictionary of words
+// cound number of words in dictionary array
+// random number
+// pick random word
+// - number of letters in word
+// - array of guessed letters
+// - 
+// game loop
+//. user picks letter
+// is letter already guessed
+// is letter in word 
+// draw word
+// draw hangman 
+// pick again
+//
+// end game routine 
